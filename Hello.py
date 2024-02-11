f2defc3ccd1551393e952a3b0e8f99716d24c9ca
alexthill1875 <159728033+alexthill1875@users.noreply.github.com>
2024-02-11 20:52:52 +0000

    Alcohol

--- Hello.py
+++ Hello.py
@@ -1,51 +1,35 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+def calculate_volume(unit, percentage):
+    return (1000 * unit) / percentage
 
+def calculate_percentage(volume, unit):
+    return unit * (1000 / volume)
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+def calculate_unit(percentage, volume):
+    return percentage / (1000 / volume)
 
-    st.write("# Welcome to Streamlit! 👋")
+def main():
+    st.title("Unit-Volume-Percentage Calculator")
 
-    st.sidebar.success("Select a demo above.")
+    choice = st.radio("Choose the value you want to calculate:", ["Volume", "Percentage", "Unit"])
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    if choice == "Volume":
+        unit_input = st.number_input("Enter unit:", min_value=0.0)
+        percentage_input = st.number_input("Enter percentage:", min_value=0.0)
+        result = calculate_volume(unit_input, percentage_input)
 
+    elif choice == "Percentage":
+        volume_input = st.number_input("Enter volume:", min_value=0.0)
+        unit_input = st.number_input("Enter unit:", min_value=0.0)
+        result = calculate_percentage(volume_input, unit_input)
+
+    elif choice == "Unit":
+        volume_input = st.number_input("Enter volume:", min_value=0.0)
+        percentage_input = st.number_input("Enter percentage:", min_value=0.0)
+        result = calculate_unit(percentage_input, volume_input)
+
+    st.write(f"The calculated value is: {result}")
 
 if __name__ == "__main__":
-    run()
+    main()
